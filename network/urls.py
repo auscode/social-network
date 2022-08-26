0414b1596e8d73323f946993fb2402c72d749bad
Harshit Mishra <76650831+auscode@users.noreply.github.com>
2022-08-26 12:34:26 +0530

    first commit

---
+++ network/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("load", views.load_posts, name="load_posts"),
+    path("save_post", views.save_post, name="save_post"),
+    path("load/followed", views.load_followed_posts, name="load_followed_posts"),
+    path("profile/<int:user_id>", views.profile, name="profile"),
+    path("post/<int:post_id>/update_like", views.update_like, name="update_like"),
+    path("profle/<int:profile_id>/update_follow",views.update_follow,name="update_follow")
+]
